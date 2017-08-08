0a7e3cf9853370b03ba6cb31b927f5e53e3cfe9b
Ilya Durdyev <funbanji@gmail.com>
2017-08-08 15:29:00 +0300

    Test doxygen generation

--- include/GitHubAPI.cpp
+++ include/GitHubAPI.cpp
@@ -6,10 +6,12 @@
 #include "GitHubAPI.h"
 #include "../src/CURLUtils.h"
 
+///@inherit
 githubapicpp::GitHubAPI::GitHubAPI(const std::string &clientId,
                                    const std::string &secret) :
         clientId(clientId),secret(secret) {}
 
+///@inherit
 githubapicpp::User githubapicpp::GitHubAPI::getUser(std::string user) {
     std::cout << CURLUtils::getUserCurl(clientId, secret, user) << std::endl;
     return githubapicpp::User();
--- include/GitHubAPI.h
+++ include/GitHubAPI.h
@@ -7,13 +7,27 @@
 
 #include "User.h"
 
+/*!
+ * GitHubAPI CPP namespace
+ */
 namespace githubapicpp {
 
+    /*!
+     * This is main githubapicpp class
+     * here are implemented GitHub API Rest V3
+     */
     class GitHubAPI {
     private:
         std::string clientId;
         std::string secret;
     public:
+        /*!
+         * Please enter here your application client_id and secret
+         * and then you can use github API. More information you can
+         * find on github website http://developer.github.com
+         * @param clientId applicatoin client_id
+         * @param secret application secret
+         */
         GitHubAPI(const std::string &clientId, const std::string &secret);
         User getUser(std::string user);
     };
