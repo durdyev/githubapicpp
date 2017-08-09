506268a253c08926ca217ce7a1086dd5cc5aeb76
Ilya Durdyev <funbanji@gmail.com>
2017-08-09 11:14:38 +0300

    started to implement organizations api

---
+++ include/Organization.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ilya Durdyev on 09.08.17.
+//
+
+#include "Organization.h"
---
+++ include/Organization.h
@@ -0,0 +1,14 @@
+//
+// Created by Ilya Durdyev on 09.08.17.
+//
+
+#ifndef GITHUBAPICPP_ORGANIZATION_H
+#define GITHUBAPICPP_ORGANIZATION_H
+
+
+class Organization {
+
+};
+
+
+#endif //GITHUBAPICPP_ORGANIZATION_H
